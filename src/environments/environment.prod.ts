d35b320373dac76131476e927a599265d833bde5
valens10 <your_email@example.com>
2025-06-01 19:57:47 +0200

    Added production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://localhost:8000',
-  HOST_NAME: 'http://localhost:8000',
+  BASE_URL: 'https://url-shortening-api-dnof.onrender.com',
+  HOST_NAME: 'https://url-shortening-api-dnof.onrender.com',
+
+  // BASE_URL: 'http://localhost:8000',
+  // HOST_NAME: 'http://localhost:8000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 
 export const environment = {
   production: false,
+  // BASE_URL: 'https://url-shortening-api-dnof.onrender.com',
+  // HOST_NAME: 'https://url-shortening-api-dnof.onrender.com',
   BASE_URL: 'http://localhost:8000',
   HOST_NAME: 'http://localhost:8000',
 };
